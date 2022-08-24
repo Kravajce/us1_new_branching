86616a62bf706ff812aaa0fffe31d1a718f1c90f
Mladen S <m1ad3n2@gmail.com>
2022-08-24 17:35:29 -0400

    problem  class created in master

---
+++ src/Login/Problem.java
@@ -0,0 +1,17 @@
+package Login;
+
+public class Problem {
+    public static void main(String[] args) {
+
+        for(int j=0;j<5;j++){
+            System.out.println(j);
+        }
+
+
+        // some code here
+        // another code here master
+
+
+
+    }
+}
